b009fbbdc23e7938a939a13bbd03b1e71f29c2a6
emmanuelwambua767 <emmmanuelwambua767@gmail.com>
2026-04-15 14:37:51 +0300

    task2

---
+++ practise.py
@@ -0,0 +1,4 @@
+num=76.88888
+t=str(num)
+r=float(t[3]+t[0:])
+print(r)
---
+++ task2.py
@@ -0,0 +1,27 @@
+# Questions create a new file
+# Convert a float to an integer with an inbuilt function in Python
+# temp = 56.8926 to 57
+temp=(56.8926)
+int=temp
+print(round(int))
+# Convert the float below to give the results as follows
+# temp = 56.8926 to 56.89 
+temp=(56.8926)
+print(round(temp,2))
+# Convert the float below to give the results as follows
+# temp = 56.8926 to 56.893 
+temp=(56.8926)
+print(round(temp,3))
+# Convert the float below to give the results as follows
+# temp=56.8926 to 8.926 
+temp=56.8926
+t=str(temp)
+r=(t[3]+t[2]+t[4:])
+print(r)
+# NB: Use string  slice & concatenation, but have result as float 
+
+# Attempt questions below. Whether you get the right answer or not, still read the solution explanation.
+# https://realpython.com/quizzes/python-data-types/
+
+
+
